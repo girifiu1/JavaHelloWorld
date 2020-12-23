512f1d4567945983be2d3d171e8eb25941fedff3
girifiu <giri_fiu@yahoo.com>
2020-12-23 12:12:56 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+
+import java.util.ArrayList;
+public class HelloWorld {
+
+
+
+    public static void main(String[] args) {
+
+        ArrayList<String> myarr = new ArrayList<String>();
+        myarr.add("Monsoon");
+        myarr.set(0,"Girish");
+
+        System.out.println();
+    }
+}
